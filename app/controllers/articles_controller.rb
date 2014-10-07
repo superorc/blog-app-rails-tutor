979a7b629f146694f93fc3aa26795fa99f445935
superorc <rykanoga@gmail.com>
2014-10-07 14:39:22 +0600

    CRUD work

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,6 +4,10 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
 
+  def edit
+    @article = Article.find(params[:id])
+  end
+
   def index
     @articles = Article.all
   end
@@ -21,8 +25,25 @@ class ArticlesController < ApplicationController
       render 'new'
     end
 
-
   end
+
+  def update
+    @article = Article.find(params[:id])
+  
+    if @article.update(article_params)
+      redirect_to @article
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @article = Article.find(params[:id])
+    @article.destroy
+  
+    redirect_to articles_path
+  end
+
   private
     def article_params
       params.require(:article).permit(:title, :text)
