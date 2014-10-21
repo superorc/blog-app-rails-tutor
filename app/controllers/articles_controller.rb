e969a0e4975c76383b4c8d68c054f412b1e3d4df
superorc <rykanoga@gmail.com>
2014-10-21 16:24:16 +0600

    del basic auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,8 +1,6 @@
 class ArticlesController < ApplicationController
 
-  http_basic_authenticate_with name: "admin", password: "admin", except: [:index, :show]
-
-  def new
+    def new
     @article = Article.new
   end
 
